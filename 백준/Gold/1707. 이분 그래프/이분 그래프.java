d9af3e7bac9d22644249f50f6204979bdb2015bf
posite <100074242+posite@users.noreply.github.com>
2024-01-19 16:56:20 +0900

    [Gold IV] Title: 이분 그래프, Time: 1092 ms, Memory: 270600 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -7,68 +7,66 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int v, e;
-	static ArrayList<Integer>[] al;
-	static int visit[];
+    private static int total, n, m;
+    private static int[] check;
+    private static ArrayList<Integer>[] al;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stz = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(stz.nextToken());
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            total = Integer.parseInt(br.readLine());
+            for (int i = 0; i < total; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                n = Integer.parseInt(st.nextToken());
+                m = Integer.parseInt(st.nextToken());
+                al = new ArrayList[n + 1];
+                check = new int[n + 1];
+                for (int j = 0; j <= n; j++) {
+                    al[j] = new ArrayList<Integer>();
+                }
+                for (int j = 0; j < m; j++) {
+                    StringTokenizer s = new StringTokenizer(br.readLine());
+                    int a = Integer.parseInt(s.nextToken());
+                    int b = Integer.parseInt(s.nextToken());
+                    al[a].add(b);
+                    al[b].add(a);
+                }
+                bfs();
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		for(int tc = 0; tc < t; tc++) {
-			stz = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(stz.nextToken());
-			e = Integer.parseInt(stz.nextToken());
-			visit = new int[v+1];
-			al = new ArrayList[v+1];
+    public static void bfs() {
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (check[i] == 0) {
+                q.add(i);
+                check[i] = 1;
+            }
+            while (!q.isEmpty()) {
+                int temp = q.poll();
+                for (int j = 0; j < al[temp].size(); j++) {
+                    if (check[al[temp].get(j)] == 0) {
+                        q.add(al[temp].get(j));
+                    }
 
-			for(int i = 0; i <= v; i++)
-				al[i] = new ArrayList<Integer>();
+                    if (check[al[temp].get(j)] == check[temp]) {
+                        System.out.println("NO");
+                        return;
+                    }
 
-			for(int i = 0; i < e; i++) {
-				stz = new StringTokenizer(br.readLine());
-				int p1 = Integer.parseInt(stz.nextToken());
-				int p2 = Integer.parseInt(stz.nextToken());
-
-				al[p1].add(p2);
-				al[p2].add(p1);
-			}
-			grouping();
-		}
-	}
-
-	public static void grouping() {
-		Queue<Integer> q = new LinkedList<Integer>();
-
-		for(int i = 1; i <= v; i++) {
-			if(visit[i] == 0) {
-				q.add(i);
-				visit[i] = 1;
-			}
-
-			while(!q.isEmpty()) {
-				int now = q.poll();
-
-				for(int j = 0; j < al[now].size(); j++) {
-					if(visit[al[now].get(j)] == 0) {
-						q.add(al[now].get(j));
-					}
-					
-					if(visit[al[now].get(j)] == visit[now]) {
-						System.out.println("NO");
-						return;
-					}
-
-					if(visit[now] == 1 && visit[al[now].get(j)] == 0)
-						visit[al[now].get(j)] = 2;
-					else if(visit[now] == 2 && visit[al[now].get(j)] == 0)
-						visit[al[now].get(j)] = 1;
-				}
-			}
-		}
-
-		System.out.println("YES");
-	}
-
+                    if (check[temp] == 1 && check[al[temp].get(j)] == 0) {
+                        check[al[temp].get(j)] = 2;
+                    } else if (check[temp] == 2 && check[al[temp].get(j)] == 0) {
+                        check[al[temp].get(j)] = 1;
+                    }
+                }
+            }
+        }
+        System.out.println("YES");
+    }
 }
+
