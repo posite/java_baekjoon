faada5241a41394504b156a466cfad98b168940b
posite <100074242+posite@users.noreply.github.com>
2024-03-16 00:33:12 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 164 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -3,86 +3,74 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 백준알고리즘 1303번 전쟁 - 전투
-
 public class Main {
-    static int N, M, nowX, nowY;
-    static char[][] arr;
-
-    static boolean[][] visit;
-    static int[] dirX = {0, 0, -1, 1}; // 상 하 좌 우
-    static int[] dirY = {-1, 1, 0, 0}; // 상 하 좌 우
-    static int sum_B, sum_W;
-
+    private static int n, m;
+    private static char[][] board;
+    private static boolean[][] check;
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, 1, -1};
+    private static int myArea, enemyArea;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new char[M][N];
-        visit = new boolean[M][N];
-
-        for (int i = 0; i < M; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = str.charAt(j);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new char[m][n];
+        check = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            String split = br.readLine();
+            for (int j = 0; j < n; j++) {
+                board[i][j] = split.charAt(j);
             }
         }
-
-        int result_W = 0;
-        int result_B = 0;
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visit[i][j] && arr[i][j] == 'W') {
-                    sum_W = 1;
-                    DFS_W(i, j);
-                    result_W += (sum_W * sum_W);
-                }
-                if (!visit[i][j] && arr[i][j] == 'B') {
-                    sum_B = 1;
-                    DFS_B(i, j);
-                    result_B += (sum_B * sum_B);
+        int myMax = 0;
+        int enemyMax = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!check[i][j]) {
+                    if (board[i][j] == 'W') {
+                        myArea = 1;
+                        dfsW(i, j);
+                        myMax += (myArea * myArea);
+                    } else {
+                        enemyArea = 1;
+                        dfsB(i, j);
+                        enemyMax += (enemyArea * enemyArea);
+                    }
                 }
             }
         }
-        System.out.println(result_W + " " + result_B);
-
+        System.out.println(myMax + " " + enemyMax);
     }
 
-    static void DFS_W(int x, int y) {
-        visit[x][y] = true;
+    public static void dfsW(int x, int y) {
+        check[x][y] = true;
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        for (int i = 0; i < 4; i++) {
-            nowX = dirX[i] + x;
-            nowY = dirY[i] + y;
-
-            if (range_check() && !visit[nowX][nowY] && arr[nowX][nowY] == 'W') {
-                sum_W++;
-                DFS_W(nowX, nowY);
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                if (!check[nx][ny] && board[nx][ny] == 'W') {
+                    myArea++;
+                    dfsW(nx, ny);
+                }
             }
         }
+    }
 
-    } // End of DFS
+    public static void dfsB(int x, int y) {
+        check[x][y] = true;
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-    static void DFS_B(int x, int y) {
-        visit[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            nowX = dirX[i] + x;
-            nowY = dirY[i] + y;
-
-            if (range_check() && !visit[nowX][nowY] && arr[nowX][nowY] == 'B') {
-                sum_B++;
-                DFS_B(nowX, nowY);
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                if (!check[nx][ny] && board[nx][ny] == 'B') {
+                    enemyArea++;
+                    dfsB(nx, ny);
+                }
             }
         }
-
-    } // End of DFS
-
-    static boolean range_check() {
-        return (nowX >= 0 && nowY >= 0 && nowX < M && nowY < N);
-    } // End of range_check
+    }
 }
