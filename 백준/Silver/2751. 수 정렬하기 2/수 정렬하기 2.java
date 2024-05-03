2dd5bd6efe2139ce5c5b5d336077a2c0977ab8e9
posite <100074242+posite@users.noreply.github.com>
2024-05-03 10:21:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1852 ms, Memory: 133076 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int number : arr) {
+            bw.write(Integer.toString(number));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
