93dbc3454a93a9f63c3a1ccfbd69ec43803580ef
posite <100074242+posite@users.noreply.github.com>
2024-05-08 10:51:53 +0900

    [level 2] Title: 피로도, Time: 0.47 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,19 @@
 import java.util.*;
 
 class Solution {
+    private static int max = 0;
     public int solution(int k, int[][] dungeons) {
-        List<List<Integer>> list = new ArrayList();
-        fatigue(k, dungeons, new ArrayList(), list);
-        int max = 0;
-        for(List<Integer> order : list) {
-            if(order.size() > max) {
-                max = order.size();
-            }
-        }
-        
+        fatigue(k, dungeons, new ArrayList());
         return max;
     }
     
-    public void fatigue(int k, int[][] dungeons, ArrayList<Integer> now, List<List<Integer>> list) {
-        list.add(new ArrayList<>(now));
+    public void fatigue(int k, int[][] dungeons, ArrayList<Integer> now) {
+        max = Math.max(now.size(), max);
         for(int i = 0; i < dungeons.length; i++) {
             //최소 피로도 확인
             if(k >= dungeons[i][0] && !now.contains(i)) {
                 now.add(i);
-                fatigue(k - dungeons[i][1], dungeons, now, list);
+                fatigue(k - dungeons[i][1], dungeons, now);
                 now.remove(now.size() - 1);
             }
         }
