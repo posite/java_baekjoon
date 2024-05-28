dcc7537557d95a9ccbfd2d79a800ef47ab3632d9
posite <100074242+posite@users.noreply.github.com>
2024-05-29 00:35:19 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.49 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -4,51 +4,49 @@ class Solution {
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
     public int[] solution(String[][] places) {
-        int[] ans = new int[5];
-        Arrays.fill(ans, 1);
+        int[] answer = {1, 1, 1, 1, 1};
         int[][] visited = new int[5][5];
         int placeIndex = 0;
         Queue<Point> queue = new LinkedList<>();
-        for (String[] place : places) {
+        for(String[] place: places) {
             //System.out.println(placeIndex);
-            for (int i = 0; i < 5; i++) {
-                if (ans[placeIndex] == 0) {
+            for(int i=0; i<5; i++) {
+                if (answer[placeIndex] == 0) {
                     break;
                 }
-                for (int j = 0; j < 5; j++) {
-                    if (ans[placeIndex] == 0) {
+                for(int j=0; j<5; j++) {
+                    if(answer[placeIndex] == 0) {
                         break;
                     }
-                    if (place[i].charAt(j) == 'P') {
+                    if(place[i].charAt(j)=='P') {
+                        //System.out.println(i + " " + j);
                         visited = new int[5][5];
                         queue.clear();
                         queue.offer(new Point(i, j));
-                        //System.out.println(i + " " + j);
-                        while (!queue.isEmpty()) {
+                        while(!queue.isEmpty()) {
                             Point current = queue.poll();
                             //System.out.println(current.x + " " + current.y + " " + visited[current.x][current.y]);
-                            if (visited[current.x][current.y] <= 2) {
+                            if(visited[current.x][current.y] <=2) {
                                 if (current.x != i || current.y != j) {
-                                    if (place[current.x].charAt(current.y) == 'P') {
-                                        //System.out.println("break " + current.x + " " + current.y + " " + visited[current.x][current.y]);
-                                        ans[placeIndex] = 0;
+                                   if(place[current.x].charAt(current.y) == 'P') {
+                                        answer[placeIndex] = 0;
                                         break;
-                                    }
+                                    } 
                                 }
+                                
                             } else {
                                 continue;
                             }
-                            for (int k = 0; k < dx.length; k++) {
+                            for(int k=0; k<4; k++) {
                                 int nx = current.x + dx[k];
                                 int ny = current.y + dy[k];
-                                if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
-                                    if (place[nx].charAt(ny) != 'X') {
-                                        if (nx != i || ny != j) {
-                                            if (visited[current.x][current.y] <= 1) {
+                                if(nx>=0 && nx<5 && ny>=0 && ny<5) {
+                                    if(place[nx].charAt(ny) != 'X') {
+                                        if(nx != i || ny != j) {
+                                            if(visited[current.x][current.y] <= 1) {
                                                 visited[nx][ny] = visited[current.x][current.y] + 1;
                                                 queue.offer(new Point(nx, ny));
                                             }
-
                                         }
                                     }
                                 }
@@ -57,15 +55,13 @@ class Solution {
                     }
                 }
             }
-            //System.out.println();
             placeIndex++;
         }
-        return ans;
+        return answer;
     }
-
+    
     static class Point {
         int x, y;
-
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
