bdeab50e33759a70632f73827206164b38c5e645
posite <100074242+posite@users.noreply.github.com>
2024-05-28 23:58:28 +0900

    [Gold III] Title: 연구소 3, Time: 364 ms, Memory: 51720 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
-    private static int max = Integer.MAX_VALUE;
+    private static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,117 +27,82 @@ public class Main {
                 board[i][j] = value;
             }
         }
-        activate(board, viruses, 0, 0, m);
-        if (max == Integer.MAX_VALUE) {
+        activate(board, viruses, new ArrayList<Point>(), 0, 0, m);
+        if(min == Integer.MAX_VALUE) {
             System.out.println(-1);
         } else {
-            System.out.println(max);
+            System.out.println(min);
         }
     }
-
-    private static void activate(int[][] board, ArrayList<Point> viruses, int start, int count, int max) {
-        if (count == max) {
-            int[][] temp = new int[board.length][board[0].length];
-            for (int i = 0; i < board.length; i++) {
-                temp[i] = board[i].clone();
+    
+    private static void activate(int[][] board, ArrayList<Point> viruses, ArrayList<Point> activated, int start, int count, int max) {
+        if(max == count) {
+            int[][] boardCopy = new int[board.length][board.length];
+            for(int i=0; i<board.length; i++) {
+                boardCopy[i] = board[i].clone();
             }
-            bfs(temp);
-            return;
+            bfs(boardCopy, activated);
         }
-
-        for (int i = start; i < viruses.size(); i++) {
+        for(int i=start; i< viruses.size(); i++) {
             Point virus = viruses.get(i);
             board[virus.x][virus.y] = 2;
-            activate(board, viruses, i + 1, count + 1, max);
+            activated.add(virus);
+            activate(board, viruses, activated, i+1, count+1, max);
             board[virus.x][virus.y] = 3;
+            activated.remove(activated.size() -1);
         }
     }
-
-    private static void bfs(int[][] board) {
-        int size = board.length;
-        int[][] ans = new int[size][size];
+    
+    private static void bfs(int[][] board, ArrayList<Point> activated) {
+        int length = board.length;
+        int[][] ans = new int[length][length];
         for (int[] an : ans) {
             Arrays.fill(an, Integer.MAX_VALUE);
         }
         Queue<Point> queue = new LinkedList<>();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (board[i][j] == 2) {
-                    ans[i][j] = 0;
-                    queue.offer(new Point(i, j));
-                }
-            }
+        for(Point virus: activated) {
+            ans[virus.x][virus.y] = 0;
+            queue.offer(virus);
         }
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             Point current = queue.poll();
-            for (int k = 0; k < dx.length; k++) {
-                int nx = current.x + dx[k];
-                int ny = current.y + dy[k];
-                if (nx >= 0 && nx < size && ny >= 0 && ny < size) {
-                    if (board[nx][ny] == 1) continue;
-                    if (ans[nx][ny] > ans[current.x][current.y] + 1) {
-                        //board[nx][ny] = 2;
-                        if (board[nx][ny] == 3) {
-                            board[nx][ny] = 2;
-                            ans[nx][ny] = ans[current.x][current.y] + 1;
-                            queue.offer(new Point(nx, ny));
-                            continue;
-                        }
-                        queue.offer(new Point(nx, ny));
+            for(int i=0; i<dx.length; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+                if(nx>=0 && nx<length && ny >=0 && ny < length) {
+                    if(board[nx][ny] == 1) continue;
+                    if(ans[nx][ny] > ans[current.x][current.y] + 1) {
                         ans[nx][ny] = ans[current.x][current.y] + 1;
+                        queue.offer(new Point(nx, ny));
                     }
-                }
+                } 
             }
         }
-       /* for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();*/
-        int cur = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (ans[i][j] == Integer.MAX_VALUE) {
-                    if (board[i][j] == 0) {
-                        //System.out.println();
+        int time = 0;
+        for(int i=0; i<length; i++) {
+            for(int j=0; j<length; j++) {
+                if(ans[i][j] == Integer.MAX_VALUE) {
+                    if(board[i][j] == 0) {
                         return;
                     }
-                    //System.out.print("* ");
                 } else {
-                    if (board[i][j] == 2) {
-                        continue;
-                    }
-                    if (ans[i][j] > cur) {
-                        cur = ans[i][j];
-                    }
-                    //System.out.print(ans[i][j] + " ");
+                   if(board[i][j] == 3 || board[i][j] == 2) {
+                       continue;
+                   } 
+                   if(ans[i][j] > time) {
+                       time = ans[i][j];
+                   }
                 }
             }
-            //System.out.println();
         }
-        /*for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (ans[i][j] == Integer.MAX_VALUE) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print(ans[i][j] + " ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println(cur);
-        System.out.println();*/
-        if (max == -1) {
-            max = cur;
+        if (min == -1) {
+            min = time;
             return;
         }
-        if (max > cur) {
-            max = cur;
+        if(min > time) {
+            min = time;
         }
     }
-
     static class Point {
         int x, y;
 
