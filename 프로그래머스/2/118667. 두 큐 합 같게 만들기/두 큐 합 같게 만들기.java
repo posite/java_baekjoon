dc31c4c15a00e5789ff9f772975a4222c9f8b7fb
posite <100074242+posite@users.noreply.github.com>
2024-05-30 23:34:09 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 3.52 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,37 +1,33 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        int[] board = new int[queue1.length + queue2.length];
+        System.arraycopy(queue1, 0, board, 0, queue1.length);
+        System.arraycopy(queue2, 0, board, queue1.length, queue2.length);
         long sum1 = 0;
         long sum2 = 0;
-        Deque<Integer> q1 = new LinkedList<>();
-        Deque<Integer> q2 = new LinkedList<>();
-        for (int num : queue1) {
+        for(int num: queue1) {
             sum1 += num;
-            q1.add(num);
         }
-        for (int num : queue2) {
+        for(int num: queue2) {
             sum2 += num;
-            q2.add(num);
         }
-        if ((sum1 + sum2) % 2 != 0) {
-            return -1;
-        }
-        for(int i=0; i<3*queue1.length; i++) {
-            if (sum1 == sum2) {
-                return i;
+        long target = (sum1 + sum2) / 2;
+        int start = 0, end = queue1.length;
+        for(int i=0; i< 3* queue1.length * 3; i++) {
+            if(start == end || end == board.length) {
+                return -1;
             }
-            else if (sum1 > sum2) {
-                int num = q1.poll();
-                q2.add(num);
-                sum1 -= num;
-                sum2 += num;
-            } else {
-                int number = q2.poll();
-                q1.add(number);
-                sum2 -= number;
-                sum1 += number;
+            if(sum1 > target) {
+                sum1 -= board[start];
+                start++;
+                continue;
             }
+            if(target > sum1) {
+                sum1 += board[end];
+                end++;
+                continue;
+            }
+            return i;
         }
         return -1;
     }
