23615dcff5fc70380e51f82c3b431fa951a9e725
posite <100074242+posite@users.noreply.github.com>
2024-05-31 20:59:06 +0900

    [level 2] Title: 피로도, Time: 0.26 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,35 @@
 import java.util.*;
 
 class Solution {
-    private static int max = 0;
     public int solution(int k, int[][] dungeons) {
-        fatigue(k, dungeons, new ArrayList());
+        boolean[] visited = new boolean[dungeons.length];
+        List<List<Integer>> list = new ArrayList();
+        backtraking(list, new ArrayList(), dungeons, k, visited);
+        int max = 0;
+        for(List<Integer> explore: list) {
+            if(explore.size() > max) {
+                max = explore.size();
+            }
+        }
         return max;
     }
     
-    public void fatigue(int k, int[][] dungeons, ArrayList<Integer> now) {
-        max = Math.max(now.size(), max);
-        for(int i = 0; i < dungeons.length; i++) {
-            //최소 피로도 확인, 던전 중복 확인
-            if(k >= dungeons[i][0] && !now.contains(i)) {
+    public void backtraking(List<List<Integer>> list, ArrayList<Integer> now, int[][] dungeons, int fatigue, boolean[] visited) {
+        //System.out.println(now);
+        list.add(new ArrayList<>(now));
+        if(now.size() == dungeons.length) {
+            return;
+        }
+        for(int i=0; i<dungeons.length; i++) {
+            int[] dungeon = dungeons[i];
+            if(fatigue >= dungeon[0] && !visited[i]) {
+                visited[i] = true;
                 now.add(i);
-                fatigue(k - dungeons[i][1], dungeons, now);
-                now.remove(now.size() - 1);
+                backtraking(list, now, dungeons, (fatigue - dungeon[1]), visited);
+                visited[i] = false;
+                now.remove(now.size() -1);
             }
         }
+        
     }
 }
