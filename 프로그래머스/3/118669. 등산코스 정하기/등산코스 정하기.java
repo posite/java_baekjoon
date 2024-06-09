314d4ffd7ffef92368b3327937bc4d95f7cacf2b
posite <100074242+posite@users.noreply.github.com>
2024-06-09 15:46:58 +0900

    [level 3] Title: 등산코스 정하기, Time: 703.09 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -3,67 +3,72 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         final int INF = Integer.MAX_VALUE;
-        int[] answer = {0, INF};
+        int[] distances = new int[n+1];
+        Arrays.fill(distances, INF);
         Map<Integer, List<Edge>> map = new HashMap<>();
         for(int i=1; i<=n; i++) {
             map.put(i, new ArrayList<>());
         }
-        Deque<Integer> queue = new ArrayDeque<>();
         for(int i=0; i<paths.length; i++) {
-            int a = paths[i][0];
-            int b = paths[i][1];
-            int weight = paths[i][2];
-            map.get(a).add(new Edge(b, weight));
-            map.get(b).add(new Edge(a, weight));
+            map.get(paths[i][0]).add(new Edge(paths[i][1], paths[i][2]));
+            map.get(paths[i][1]).add(new Edge(paths[i][0], paths[i][2]));
         }
-        int[] visited;
-        visited = new int[n+1];
-        Arrays.fill(visited, INF); 
-        for(int gate: gates) {
-            queue.add(gate);
-            visited[gate] = 0;
+        Queue<Entry> pq = new PriorityQueue<>();
+        for(int start: gates) {
+            distances[start] = 0;
+            pq.add(new Entry(start, 0));
         }
-        while(!queue.isEmpty()) {
-            int current = queue.poll();
+        while(!pq.isEmpty()) {
+            Entry current = pq.remove();
             boolean isSummit = false;
             for(int summit: summits) {
-                if(current == summit) {
+                if(summit == current.node) {
                     isSummit = true;
                     break;
                 }
             }
             if(isSummit) continue;
-            for(Edge edge: map.get(current)) {
-                int dest = edge.destination;
-                int weight = Math.max(edge.weight, visited[current]);
-                if(visited[dest] > weight) {
-                    visited[dest] = weight;
-                    queue.add(dest);
-                    //System.out.println(dest + " " + weight);
+            if(current.distance > distances[current.node]) continue;
+            for(Edge edge: map.get(current.node)) {
+                int edgeMax = Math.max(current.distance, edge.weight);
+                if(distances[edge.destination] > edgeMax) {
+                    distances[edge.destination] = edgeMax;
+                    pq.add(new Entry(edge.destination, edgeMax));
                 }
             }
         }
-        int intensity = Integer.MAX_VALUE;
+        int min = INF;
         int index = 0;
-        for(int i=0; i<summits.length; i++) {
-            int summit = summits[i];
-            if(intensity > visited[summit]) {
+        for(int summit: summits) {
+            if(min > distances[summit]) {
+                min = distances[summit];
                 index = summit;
-                intensity = visited[summit];
-            }else if(intensity == visited[summit]) {
+            } else if(min == distances[summit]) {
                 if(index > summit) {
                     index = summit;
                 }
             }
         }
-        answer[0] = index;
-        answer[1] = intensity;
-        return answer;
+        
+        return new int[]{index, min};
+    }
+    
+    public class Entry implements Comparable<Entry> {
+        int node, distance;
+        public Entry(int node, int distance)  {
+            this.node = node;
+            this.distance = distance;
+        }
+        
+        @Override
+        public int compareTo(Entry o) {
+            return this.distance - o.distance;
+        }
     }
     
     public class Edge {
         int destination, weight;
-        public Edge(int destination, int weight) {
+        public Edge(int destination, int weight)  {
             this.destination = destination;
             this.weight = weight;
         }
