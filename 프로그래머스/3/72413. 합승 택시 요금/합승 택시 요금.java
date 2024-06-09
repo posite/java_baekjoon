4e4eeb58539f3e2be8692eed9aff5c0f439cc68d
posite <100074242+posite@users.noreply.github.com>
2024-06-09 18:37:28 +0900

    [level 3] Title: 합승 택시 요금, Time: 2.86 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         final int INF = Integer.MAX_VALUE;
         int min = INF;
-        int[][] graph = new int[n + 1][n + 1];
+        int[][] graph = new int[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             Arrays.fill(graph[i], INF);
         }
@@ -12,66 +12,78 @@ class Solution {
             graph[fare[0]][fare[1]] = fare[2];
             graph[fare[1]][fare[0]] = fare[2];
         }
-        int[] fees = new int[n + 1];
+        int[] startDistance = new int[n+1];
+        int[] aDistance = new int[n+1];
+        int[] bDistance = new int[n+1];
+        Arrays.fill(startDistance, INF);
+        Arrays.fill(aDistance, INF);
+        Arrays.fill(bDistance, INF);
+        
         Queue<Point> pq = new PriorityQueue<>();
         pq.add(new Point(s, 0));
-        Arrays.fill(fees, INF);
-        fees[s] = 0;
+        startDistance[s] = 0;
         while(!pq.isEmpty()) {
             Point current = pq.remove();
-            if(current.fee > fees[current.node]) continue;
-            int fee = current.fee;
-            int node = current.node;
-            //System.out.println(node + " " + fee + " ");
-            for(int i=0; i<=n; i++) {
-                if(graph[node][i] != INF && fees[i] > fees[node] + graph[node][i]) {
-                    pq.add(new Point(i, fees[node] + graph[node][i]));
-                    fees[i] = fees[node] + graph[node][i];
-                    //System.out.println(node + " " + fees[node] + " ");
-                } 
-            }
-        }
-        //System.out.println(Arrays.toString(fees));
-        for(int i=1; i<=n; i++) {
-            if(i == s) {
-                min = Math.min(min, fees[a] + fees[b]);
-                continue;
-            }
-            int[] weights = new int[n + 1];
-            Arrays.fill(weights, INF);
-            pq.add(new Point(i, 0));
-            weights[i] = 0;
-            while(!pq.isEmpty()) {
-                Point current = pq.remove();
-                int fee = current.fee;
-                int node = current.node;
-                if(fee > weights[node]) continue;
-                //System.out.println(node + " " + fee + " ");
-                for(int j=0; j<=n; j++) {
-                    if(graph[node][j] != INF && weights[j] > weights[node] + graph[node][j]) {
-                        pq.add(new Point(j, weights[node] + graph[node][j]));
-                        weights[j] = weights[node] + graph[node][j];
-                    } 
+            if(current.distance > startDistance[current.node]) continue;
+            for(int i=1; i<=n; i++) {
+                if(graph[current.node][i] != INF) {
+                    int nextDistance = current.distance + graph[current.node][i];
+                    if(startDistance[i] > nextDistance) {
+                        pq.add(new Point(i, nextDistance));
+                        startDistance[i] = nextDistance;
+                    }
                 }
-            
             }
-            //System.out.println(weights[a] + " " + weights[b] + " " + fees[i]);
-            min = Math.min(min, weights[a] + weights[b] + fees[i]);
         }
         
+        pq.add(new Point(a, 0));
+        aDistance[a] = 0;
+        while(!pq.isEmpty()) {
+            Point current = pq.remove();
+            if(current.distance > aDistance[current.node]) continue;
+            for(int i=1; i<=n; i++) {
+                if(graph[current.node][i] != INF) {
+                    int nextDistance = current.distance + graph[current.node][i];
+                    if(aDistance[i] > nextDistance) {
+                        pq.add(new Point(i, nextDistance));
+                        aDistance[i] = nextDistance;
+                    }
+                }
+            }
+        }
+        
+        
+        pq.add(new Point(b, 0));
+        bDistance[b] = 0;
+        while(!pq.isEmpty()) {
+            Point current = pq.remove();
+            if(current.distance > bDistance[current.node]) continue;
+            for(int i=1; i<=n; i++) {
+                if(graph[current.node][i] != INF) {
+                    int nextDistance = current.distance + graph[current.node][i];
+                    if(bDistance[i] > nextDistance) {
+                        pq.add(new Point(i, nextDistance));
+                        bDistance[i] = nextDistance;
+                    }
+                }
+            }
+        }
+        for(int i=1; i<=n; i++) {
+            min = Math.min(min, startDistance[i] + aDistance[i] + bDistance[i]);
+        }
         return min;
     }
     
     public class Point implements Comparable<Point> {
-        int node, fee;
-        public Point(int node, int fee) {
+        int node, distance;
+        public Point(int node, int distance) {
             this.node = node;
-            this.fee = fee;
+            this.distance = distance;
         }
         
         @Override
         public int compareTo(Point o) {
-            return this.fee - o.fee;
+            return this.distance - o.distance;
         }
     }
 }
