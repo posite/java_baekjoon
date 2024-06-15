639d1795ae49b451c5ea27f3614feb0171788945
posite <100074242+posite@users.noreply.github.com>
2024-06-15 14:36:35 +0900

    [level 2] Title: 문자열 압축, Time: 12.24 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,74 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        if(s.length() == 1) {
-            return 1;
-        }
-        int minLength = Integer.MAX_VALUE;
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 1; i <= s.length() / 2; i++) {
-            int currentLength = 0;
-            int currentIndex = 0;
-            //System.out.println("split " + i);
-            while (s.length() > currentIndex) {
-                String currentString = "";
-                if (currentIndex + i > s.length()) {
-                    currentString = s.substring(currentIndex);
-                } else {
-                    currentString = s.substring(currentIndex, currentIndex + i);
-                }
-                //System.out.println(currentIndex + " ~ " + (currentIndex + i) + " " + currentString + " ");
-                currentIndex += i;
-                if (map.containsKey(currentString)) {
-                    //System.out.println("add " + currentString);
-                    map.put(currentString, map.get(currentString) + 1);
+        int answer = s.length();
+        for(int i=1; i<=s.length()/2; i++) {
+            StringBuilder base = new StringBuilder();
+            String prev = s.substring(0, i);
+            int repeatCount = 1;
+            for(int j=i; j<=s.length(); j+=i) {
+                String current;
+                if(j+i > s.length()) current = s.substring(j);
+                else current = s.substring(j, j+i);
+                if(current.equals(prev)) {
+                    repeatCount++;
                     continue;
                 }
-                if (map.isEmpty()) {
-                    //System.out.println("put " + currentString);
-                    map.put(currentString, 1);
-                    continue;
-                }
-                //System.out.println("put " + currentString);
-                map.put(currentString, 1);
-                String prevString = s.substring(currentIndex - 2 * i, currentIndex - i);
-                //System.out.println("prev " + (currentIndex - i) + " ~ " + currentIndex + " " + prevString);
-                int prevCount = map.remove(prevString);
-                //System.out.println(map);
-                if (prevCount > 1) {
-                    currentLength += (Integer.toString(prevCount).length() + i);
-                    //System.out.println("remove " + prevString + " currentLength " + currentLength);
-                    continue;
-                }
-                currentLength += i;
-                //System.out.println("remove " + prevString + " currentLength " + currentLength);
+                if(repeatCount > 1) base.append(repeatCount + prev);
+                else base.append(prev);
+                repeatCount = 1;
+                prev = current;
             }
-            /*minLength = Math.min(currentLength, minLength);
-            System.out.println(minLength);*/
-            //System.out.println(map);
-            if (!map.isEmpty()) {
-                int remain = 0;
-                if (i == 1) {
-                    remain = s.length() - 1;
-                } else if (s.length() % i != 0) {
-                    remain = s.length() - s.length() % i;
-                } else {
-                    remain = s.length() - i;
-                }
-                //System.out.print("remain " + remain + " ");
-                String lastString = s.substring(remain);
-                //System.out.println("remove  last " + lastString);
-                int count = map.remove(lastString);
-                if (count > 1) {
-                    currentLength += Integer.toString(count).length() + lastString.length();
-                } else {
-                    currentLength += lastString.length();
-                }
-            }
-            minLength = Math.min(currentLength, minLength);
-            //System.out.println(minLength);
+            if(repeatCount > 1) base.append(repeatCount + prev);
+            else base.append(prev);
+            answer = Math.min(answer, base.length());
         }
-        return minLength;
+        
+        return answer;
     }
 }
