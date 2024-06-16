18b83d196507e24e7b564029e65728e0033218ef
posite <100074242+posite@users.noreply.github.com>
2024-06-16 15:55:01 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.92 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,33 +1,31 @@
 class Solution {
     public String solution(String new_id) {
         StringBuilder answer = new StringBuilder(new_id.toLowerCase());
-        for(int i=0; i< answer.length(); i++) {
+        for(int i=0; i<answer.length(); i++) {
             char current = answer.charAt(i);
             if(Character.isDigit(current) || Character.isLowerCase(current)) continue;
-            if(current == '-' || current == '_' || current == '.') continue;
+            if(current == '.' || current == '_' || current == '-') continue;
             answer.deleteCharAt(i);
             i--;
         }
-        
         while(answer.indexOf("..") != -1) {
-            answer.deleteCharAt(answer.indexOf("..") +1);
+            answer.deleteCharAt(answer.indexOf(".."));
         }
         
         if(answer.charAt(0) == '.') answer.deleteCharAt(0);
-        if (answer.length() > 0) {
-            if(answer.charAt(answer.length() -1) == '.') answer.deleteCharAt(answer.length() -1);
-        }
+        if(answer.length() > 0 && answer.charAt(answer.length()-1) == '.') answer.deleteCharAt(answer.length()-1);
         
-        if(answer.length() == 0 ) answer.append("a");
+        if(answer.length() == 0) answer.append("a");
+        
         if(answer.length() > 15) answer.replace(15, answer.length(), "");
-        if(answer.charAt(0) == '.') answer.deleteCharAt(0);
-        if (answer.length() > 0) {
-            if(answer.charAt(answer.length() -1) == '.') answer.deleteCharAt(answer.length() -1);
-        }
-        
-        while(answer.length() < 3) {
+        if(answer.length() > 0 && answer.charAt(0) == '.') answer.deleteCharAt(0);
+        if(answer.length() > 0 && answer.charAt(answer.length()-1) == '.') answer.deleteCharAt(answer.length()-1);
+        while(answer.length()<3) {
             answer.append(answer.charAt(answer.length() -1));
         }
+        
+        
+        
         return answer.toString();
     }
 }
