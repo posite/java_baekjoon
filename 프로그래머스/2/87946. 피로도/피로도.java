9272111c1dcaa1fb4f68fdf810b0a94389348a8a
posite <100074242+posite@users.noreply.github.com>
2024-07-11 20:18:55 +0900

    [level 2] Title: 피로도, Time: 0.23 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,28 +1,33 @@
 import java.util.*;
 
 class Solution {
-    int max = 0;
     public int solution(int k, int[][] dungeons) {
+        List<List<Integer>> list = new ArrayList();
         boolean[] visited = new boolean[dungeons.length];
-        exploreDungeon(new ArrayList<>(), dungeons, k, visited);
+        fatigue(list, new ArrayList(), dungeons, visited, k);
+    
+        int max = 0;
+        //System.out.println(list);
+        for(int i=0; i<list.size(); i++) {
+            if(list.get(i).size() > max) {
+                max = list.get(i).size();
+            }
+        }
         return max;
     }
     
-    public void exploreDungeon(List<Integer> now, int[][] dungeons, int health, boolean[] visited) {
-        max = Math.max(max, now.size());
-        for(int i=0; i<dungeons.length; i++) {
-            if(visited[i]) {
-                continue;
-            }
-            if(dungeons[i][0] > health) {
-                continue;
-            }
-            now.add(i);
-            visited[i] = true;
-            exploreDungeon(now, dungeons, health - dungeons[i][1], visited);
-            now.remove(now.size()-1);
-            visited[i] = false;
-        }
-    }
+    public void fatigue(List<List<Integer>> list, List<Integer> now, int[][] dungeons, boolean[] visited, int health) {
+        list.add(new ArrayList<>(now));
         
+        for(int i=0; i<dungeons.length; i++) {
+            if(!visited[i] && health >= dungeons[i][0]) {
+                now.add(i);
+                visited[i] = true;
+                fatigue(list, now, dungeons, visited, health - dungeons[i][1]);
+                now.remove(now.size() -1);
+                visited[i] = false;
+            }
+        }
+        
+    }
 }
