4e8f2ea7469311a2b0b010cd21025f46e4fd8009
posite <100074242+posite@users.noreply.github.com>
2024-07-18 20:30:35 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 13.23 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -5,33 +5,30 @@ class Solution {
         int[] totalQueue = new int[queue1.length + queue2.length];
         System.arraycopy(queue1, 0, totalQueue, 0, queue1.length);
         System.arraycopy(queue2, 0, totalQueue, queue1.length, queue2.length);
-        //System.out.println(Arrays.toString(totalQueue));
-        long sumQ1 = 0;
-        long sumQ2 = 0;
-        for(int i=0; i<queue1.length; i++) {
-            sumQ1 += queue1[i];
-        }
-        for(int i=0; i<queue2.length; i++) {
-            sumQ2 += queue2[i];
-        }
-        int left = 0, right = queue1.length;
-        long target = (sumQ1 + sumQ2)/2;
-        //System.out.println(sumQ1 + " " + target);
         
-        for(int i=0; i<queue1.length*3; i++) {
-            if(left == right || right == totalQueue.length) return -1;
-            
-            
-            if(sumQ1 == target) return i;
-            if(sumQ1 > target) {
-                sumQ1 -= totalQueue[left];
-                left++;
+        long sum = 0;
+        for(int i=0; i< queue1.length; i++) {
+            sum += queue1[i];
+        }
+        long sumQ1 = sum;
+        //System.out.println(sumQ1);
+        for(int i=0; i<queue2.length; i++) {
+            sum += queue2[i];
+        }
+        long mid = sum/2;
+        int start = 0, end = queue1.length;
+        //System.out.println(mid);
+        for(int i=0; i<queue1.length * 3; i++) {
+            if(start == end || end == totalQueue.length) return -1;
+            if(sumQ1 == mid) return i;
+            if(sumQ1 > mid) {
+                sumQ1 -= totalQueue[start];
+                start++;
             } else {
-                sumQ1 += totalQueue[right];
-                right++;
+                sumQ1 += totalQueue[end];
+                end++;
             }
         }
-        
         
         return -1;
     }
