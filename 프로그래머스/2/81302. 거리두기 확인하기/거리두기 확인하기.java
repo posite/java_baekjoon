79a34b6fe0bb30574536cd45d49646c568ef751e
posite <100074242+posite@users.noreply.github.com>
2024-07-24 14:00:49 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.16 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,48 +1,32 @@
 import java.util.*;
 
 class Solution {
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
     public int[] solution(String[][] places) {
-        int[] answer = {1, 1, 1, 1, 1};
-        for(int i=0; i<5; i++) {
-            bfs(places[i], answer, i);
-        }
-        return answer;
-    }
-    
-    public void bfs(String[] place, int[] answer, int now) {
-        int[][] visited = new int[5][5];
-        Deque<Point> queue = new ArrayDeque<>();
-        for(int i=0; i<5; i++) {
+        int[] answer = new int[5];
+        Deque<int[]> queue = new ArrayDeque<>();
+        outer: for(int i=0; i<5; i++) {
+            String[] place = places[i];
             for(int j=0; j<5; j++) {
-                if(place[i].charAt(j) == 'P') {
-                    for(int k=0; k<5; k++) {
-                        Arrays.fill(visited[k], 10);
-                    }
-                    visited[i][j] = 0;
-                    queue.offer(new Point(i, j));
-                    while(!queue.isEmpty()) {
-                        Point current = queue.poll();
-                        //System.out.println(current.x + " " + current.x + " " + now);
-                        if(visited[current.x][current.y] <= 2) {
-                            if(place[current.x].charAt(current.y) == 'P') {
-                                if(current.x != i || current.y != j) {
-                                    answer[now] = 0;
-                                    return;
-                                }  
+                for(int k=0; k<5; k++) {
+                    if(place[j].charAt(k) == 'P') {
+                        queue.clear();
+                        queue.offer(new int[]{j, k, 0});
+                        while(!queue.isEmpty()) {
+                            int[] current = queue.poll();
+                            if(place[current[0]].charAt(current[1]) == 'P' && current[2] != 0) {
+                                answer[i] = 0;
+                                continue outer;
                             }
-                        } else {
-                            continue;
-                        }
-                        for(int k=0; k<dx.length; k++) {
-                            int nx = current.x + dx[k];
-                            int ny = current.y + dy[k];
-                            if(nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
-                                if(place[nx].charAt(ny) != 'X') {
-                                    if(visited[nx][ny] > visited[current.x][current.y] + 1) {
-                                        visited[nx][ny] = visited[current.x][current.y] + 1;
-                                        queue.offer(new Point(nx, ny));
+                            if(current[2] == 2) continue;
+                            for(int l=0; l<4; l++) {
+                                int nx = current[0] + dx[l];
+                                int ny = current[1] + dy[l];
+                                if(nx == j && ny == k) continue;
+                                if(nx >= 0 && nx < 5 && ny >=0 && ny < 5) {
+                                    if(place[nx].charAt(ny) != 'X') {
+                                        queue.offer(new int[]{nx, ny, current[2]+1});
                                     }
                                 }
                             }
@@ -50,14 +34,9 @@ class Solution {
                     }
                 }
             }
+            answer[i] = 1;
         }
-    }
-    
-    static class Point {
-        int x, y;
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        
+        return answer;
     }
 }
