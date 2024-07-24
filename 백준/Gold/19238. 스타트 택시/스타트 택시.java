e9524705b37b1d56dc9c22ff2df4e9bdb287ce16
posite <100074242+posite@users.noreply.github.com>
2024-07-24 11:54:54 +0900

    [Gold II] Title: 스타트 택시, Time: 248 ms, Memory: 28080 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     private static int[] dx = {-1, 1, 0, 0};
@@ -13,159 +8,126 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int size = Integer.parseInt(st.nextToken());
+        int numOfCustomer = Integer.parseInt(st.nextToken());
         int fuel = Integer.parseInt(st.nextToken());
-        int[][] map = new int[n][n];
-        int[][] visited = new int[n][n];
-        ArrayList<Point> start = new ArrayList<>();
-        ArrayList<Point> dest = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        int[][] board = new int[size][size];
+        int[][] distances = new int[size][size];
+        List<Point> customerStartPoint = new ArrayList<>();
+        List<Point> customerDestinationPoint = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < size; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         st = new StringTokenizer(br.readLine());
-        int carX = Integer.parseInt(st.nextToken()) - 1;
-        int carY = Integer.parseInt(st.nextToken()) - 1;
-        for (int i = 0; i < m; i++) {
+        int startX = Integer.parseInt(st.nextToken()) - 1;
+        int startY = Integer.parseInt(st.nextToken()) - 1;
+        for (int i = 0; i < numOfCustomer; i++) {
             st = new StringTokenizer(br.readLine());
-            int startX = Integer.parseInt(st.nextToken());
-            int startY = Integer.parseInt(st.nextToken());
-            int destX = Integer.parseInt(st.nextToken());
-            int destY = Integer.parseInt(st.nextToken());
-            start.add(new Point(startX - 1, startY - 1));
-            dest.add(new Point(destX - 1, destY - 1));
-            /*map[startX - 1][startY - 1] = (i + 1) * 2;
-            map[destX - 1][destY - 1] = (i + 1) * (-2);*/
+            int cStartX = Integer.parseInt(st.nextToken()) - 1;
+            int cStartY = Integer.parseInt(st.nextToken()) - 1;
+            customerStartPoint.add(new Point(cStartX, cStartY));
+
+            int cDestinationX = Integer.parseInt(st.nextToken()) - 1;
+            int cDestinationY = Integer.parseInt(st.nextToken()) - 1;
+            customerDestinationPoint.add(new Point(cDestinationX, cDestinationY));
         }
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(carX, carY));
-        for (int i = 0; i < m; i++) {
+
+        Deque<Point> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < numOfCustomer; i++) {
+            distances = new int[size][size];
+            queue = new ArrayDeque<>();
+            queue.offer(new Point(startX, startY));
             while (!queue.isEmpty()) {
                 Point current = queue.poll();
-                //System.out.println(current.x + " " + current.y);
-                for (int j = 0; j < dx.length; j++) {
+                for (int j = 0; j < 4; j++) {
                     int nx = current.x + dx[j];
                     int ny = current.y + dy[j];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                        if (map[nx][ny] == 1) continue;
-                        if (visited[nx][ny] == 0) {
-                            if (nx != carX || ny != carY) {
-                                visited[nx][ny] = visited[current.x][current.y] + 1;
-                                queue.add(new Point(nx, ny));
-                                continue;
-                            }
-                        }
-                        if (visited[nx][ny] > visited[current.x][current.y] + 1) {
-                            visited[nx][ny] = visited[current.x][current.y] + 1;
-                            queue.add(new Point(nx, ny));
+                    if (nx >= 0 && nx < size && ny >= 0 && ny < size) {
+                        if (board[nx][ny] == 1) continue;
+                        if (nx == startX && ny == startY) continue;
+                        if (distances[nx][ny] == 0 || distances[nx][ny] > distances[current.x][current.y] + 1) {
+                            distances[nx][ny] = distances[current.x][current.y] + 1;
+                            queue.offer(new Point(nx, ny));
                         }
                     }
                 }
             }
-            /*for (int[] times : visited) {
-                for (int time : times) {
-                    System.out.print(time + " ");
+            /*for (int j = 0; j < size; j++) {
+                System.out.println(Arrays.toString(distances[j]));
+            }*/
+
+            Point nextStart = null;
+            Point nextDestination;
+            int index = -1;
+            int distance = Integer.MAX_VALUE;
+            for (int j = 0; j < customerStartPoint.size(); j++) {
+                Point customer = customerStartPoint.get(j);
+                if (distance > distances[customer.x][customer.y]) {
+                    nextStart = customer;
+                    index = j;
+                    distance = distances[customer.x][customer.y];
+                    continue;
                 }
-                System.out.println();
-            }
-            System.out.println();*/
-            int shortest = Integer.MAX_VALUE;
-            int index = 0;
-            for (int j = 0; j < start.size(); j++) {
-                Point startC = start.get(j);
-                if (shortest == visited[startC.x][startC.y]) {
-                    Point prev = start.get(index);
-                    if (prev.x > startC.x) {
+                if (distance == distances[customer.x][customer.y]) {
+                    if (nextStart.x > customer.x) {
+                        nextStart = customer;
                         index = j;
                         continue;
                     }
-                    if (prev.x == startC.x) {
-                        if (prev.y > startC.y) {
-                            index = j;
-                            continue;
-                        }
+                    if (nextStart.x == customer.x && nextStart.y > customer.y) {
+                        nextStart = customer;
+                        index = j;
                     }
-                    continue;
-                }
-                if (shortest > visited[startC.x][startC.y]) {
-                    index = j;
-                    shortest = visited[startC.x][startC.y];
                 }
             }
-            Point startPoint = start.get(index);
-            if (shortest == 0) {
-                if (startPoint.x != carX || startPoint.y != carY) {
-                    //System.out.println("도착 불가");
+            //System.out.println(distance);
+            if (distance == 0) {
+                if (nextStart.x != startX && nextStart.y != startY) {
                     System.out.println("-1");
                     return;
                 }
             }
-            //System.out.println("index: " + index);
-            //System.out.println("start " + startPoint.x + " " + startPoint.y);
-            Point destPoint = dest.get(index);
-            carX = destPoint.x;
-            carY = destPoint.y;
-            //System.out.println("dest " + destPoint.x + " " + destPoint.y);
-            visited = new int[n][n];
-            queue.offer(startPoint);
+            startX = nextStart.x;
+            startY = nextStart.y;
+            nextDestination = customerDestinationPoint.get(index);
+            distances = new int[size][size];
+            queue = new ArrayDeque<>();
+            queue.offer(new Point(startX, startY));
             while (!queue.isEmpty()) {
                 Point current = queue.poll();
-                //System.out.println(current.x + " " + current.y);
-                for (int j = 0; j < dx.length; j++) {
+                if (current.x == nextDestination.x && current.y == nextDestination.y) break;
+                for (int j = 0; j < 4; j++) {
                     int nx = current.x + dx[j];
                     int ny = current.y + dy[j];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                        if (map[nx][ny] == 1) continue;
-                        if (visited[nx][ny] == 0) {
-                            if (nx != startPoint.x || ny != startPoint.y) {
-                                visited[nx][ny] = visited[current.x][current.y] + 1;
-                                queue.add(new Point(nx, ny));
-                                continue;
-                            }
-                        }
-                        if (visited[nx][ny] > visited[current.x][current.y] + 1) {
-                            visited[nx][ny] = visited[current.x][current.y] + 1;
-                            queue.add(new Point(nx, ny));
+                    if (nx >= 0 && nx < size && ny >= 0 && ny < size) {
+                        if (board[nx][ny] == 1) continue;
+                        if (nx == startX && ny == startY) continue;
+                        if (distances[nx][ny] == 0 || distances[nx][ny] > distances[current.x][current.y] + 1) {
+                            distances[nx][ny] = distances[current.x][current.y] + 1;
+                            queue.offer(new Point(nx, ny));
                         }
                     }
                 }
             }
-            /*for (int[] times : visited) {
-                for (int time : times) {
-                    System.out.print(time + " ");
-                }
-                System.out.println();
-            }
-            System.out.println();*/
-            if (visited[destPoint.x][destPoint.y] == 0) {
-                //System.out.println("도착 불가");
+            if (distances[nextDestination.x][nextDestination.y] == 0) {
                 System.out.println("-1");
                 return;
             }
-            /*for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    System.out.print(visited[j][k] + " ");
-                }
-                System.out.println();
-            }
-            System.out.println(shortest + " " + visited[dest.x][dest.y]);*/
-            //System.out.println("to customer " + shortest + "  destination " + visited[destPoint.x][destPoint.y]);
-            if (fuel < shortest + visited[destPoint.x][destPoint.y]) {
+            if (fuel < distance + distances[nextDestination.x][nextDestination.y]) {
                 //System.out.println("연료 부족");
                 System.out.println("-1");
                 return;
             }
-            fuel = fuel - shortest + visited[destPoint.x][destPoint.y];
-            start.remove(index);
-            dest.remove(index);
-            visited = new int[n][n];
-            /*System.out.println();
-            System.out.println("fuel " + fuel);
-            System.out.println();*/
-            queue.offer(destPoint.clone());
+            fuel = fuel - distance + distances[nextDestination.x][nextDestination.y];
+            startX = nextDestination.x;
+            startY = nextDestination.y;
+            customerStartPoint.remove(index);
+            customerDestinationPoint.remove(index);
         }
         System.out.println(fuel);
     }
@@ -176,10 +138,6 @@ public class Main {
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        protected Point clone() {
-            return new Point(x, y);
         }
     }
 }
