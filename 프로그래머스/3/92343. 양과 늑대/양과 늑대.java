b35a22869506da3ed90122eeb82d9cdde1dd687c
posite <100074242+posite@users.noreply.github.com>
2024-08-04 14:07:32 +0900

    [level 3] Title: 양과 늑대, Time: 1.66 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,15 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] info, int[][] edges) {
         boolean[] visited = new boolean[info.length];
         visited[0] = true;
-        int max = sheepWolf(visited, info, edges, 1, 0);
-        return max;
+        return sheepWolf(info, edges, visited, 1, 0);
     }
     
-    public int sheepWolf(boolean[] visited, int[] info, int[][] edges, int sheep, int wolf) {
-        if (sheep == wolf) return sheep;
+    public int sheepWolf(int[] info, int[][] edges, boolean[] visited, int sheep, int wolf) {
+        if(sheep == wolf) return sheep;
         int max = sheep;
         for(int[] edge: edges) {
             int parent = edge[0];
@@ -17,9 +14,9 @@ class Solution {
             if(visited[parent] && !visited[child]) {
                 visited[child] = true;
                 if(info[child] == 0) {
-                    max = Math.max(max, sheepWolf(visited, info, edges, sheep+1, wolf));
+                    max = Math.max(max, sheepWolf(info, edges, visited, sheep+1, wolf));
                 } else {
-                    max = Math.max(max, sheepWolf(visited, info, edges, sheep, wolf+1));
+                    max = Math.max(max, sheepWolf(info, edges, visited, sheep, wolf+1));
                 }
                 visited[child] = false;
             }
