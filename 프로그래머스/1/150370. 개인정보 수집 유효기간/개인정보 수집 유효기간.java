b728c292d484941df5cc41a9a5e7e3eff8ace3cd
posite <100074242+posite@users.noreply.github.com>
2024-08-06 10:54:24 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.74 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,33 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answers = new ArrayList();
-        StringTokenizer st = new StringTokenizer(today, ".");
-        long todayValue = Long.parseLong(st.nextToken()) * 12 * 28 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
-    
         Map<String, Long> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(today, ".");
+        long currentDay = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
         for(String term: terms) {
             st = new StringTokenizer(term);
-            map.put(st.nextToken(), Long.parseLong(st.nextToken())*28);
+            map.put(st.nextToken(), Long.parseLong(st.nextToken()));
         }
-        
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<privacies.length; i++) {
-            String privacy = privacies[i];
-            st = new StringTokenizer(privacy);
-            String start = st.nextToken();
-            String termType = st.nextToken();
+            st = new StringTokenizer(privacies[i]);
+            String date = st.nextToken();
+            String type = st.nextToken();
             
-            st = new StringTokenizer(start, ".");
-            long startValue = Long.parseLong(st.nextToken()) * 12 * 28 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + map.get(termType) -1;
-            
-            if(todayValue > startValue) {
-                answers.add(i+1);
-            }
+            st = new StringTokenizer(date, ".");
+            long resultDate = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + map.get(type) * 28;
+            if(currentDay >= resultDate) list.add(i+1);      
         }
         
-        int[] answer = new int[answers.size()];
-        for(int i=0; i<answers.size(); i++) {
-            answer[i] = answers.get(i);
+        
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
         return answer;
     }
