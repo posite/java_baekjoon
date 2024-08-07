da1cde03f835da9eb4fa2376b03eb0c9a4ebac56
posite <100074242+posite@users.noreply.github.com>
2024-08-07 11:06:04 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.54 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        Map<String, Long> map = new HashMap<>();
-        StringTokenizer st = new StringTokenizer(today, ".");
-        long currentDay = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
+        Map<String, Long> typeMap = new HashMap<>();
+        StringTokenizer st;
         for(String term: terms) {
             st = new StringTokenizer(term);
-            map.put(st.nextToken(), Long.parseLong(st.nextToken()));
+            typeMap.put(st.nextToken(), Long.parseLong(st.nextToken()));
         }
+        st = new StringTokenizer(today, ".");
+        long todayValue = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
+        
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<privacies.length; i++) {
             st = new StringTokenizer(privacies[i]);
             String date = st.nextToken();
             String type = st.nextToken();
-            
             st = new StringTokenizer(date, ".");
-            long resultDate = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + map.get(type) * 28;
-            if(currentDay >= resultDate) list.add(i+1);      
+            
+            long dateValue = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + typeMap.get(type) * 28;
+            if(todayValue >= dateValue) {
+                list.add(i+1);
+            }
         }
-        
-        
-        
         int[] answer = new int[list.size()];
         for(int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
