f4c45ef2f24c542cfa050e8b2fd1a2d2e47434d8
posite <100074242+posite@users.noreply.github.com>
2024-08-07 10:49:37 +0900

    [level 1] Title: 신고 결과 받기, Time: 149.02 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,37 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-           
-        Map<String, Set<String>> map = new HashMap<>();
-        Map<String, Integer> indexes = new HashMap<>();
+        StringTokenizer st;
+        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> idMap = new HashMap<>();
         for(int i=0; i<id_list.length; i++) {
-            indexes.put(id_list[i], i);
-            map.put(id_list[i], new HashSet<>());
+            idMap.put(id_list[i], i);
+            reportMap.put(id_list[i], new HashSet<>());
         }
         for(String rep: report) {
-            StringTokenizer st = new StringTokenizer(rep);
+            st = new StringTokenizer(rep);
             String reporter = st.nextToken();
-            String opponent = st.nextToken();
-            map.get(opponent).add(reporter);
+            String user = st.nextToken();
+            reportMap.get(user).add(reporter);
         }
         
-        for(Map.Entry<String, Set<String>> entry: map.entrySet()) {
-            Set<String> value = entry.getValue();
-            if(value.size() >= k) {
-                for(String user: value) {
-                    answer[indexes.get(user)]++;
-                }
+        int[] answer = new int[id_list.length];
+        for(Set<String> reporters: reportMap.values()) {
+            if(reporters.size() >= k) {
+                for(String reporter: reporters) answer[idMap.get(reporter)]++;
             }
         }
         
         return answer;
-    }
-    
-    public int findUser(String[] id_list, String userId) {
-        for(int i=0; i<id_list.length; i++) {
-            if(id_list[i].equals(userId)) return i;
-        }
-        return -1;
     }
 }
