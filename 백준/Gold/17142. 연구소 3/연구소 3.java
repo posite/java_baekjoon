7472c65eb1b22f36cafdb414f49281bd85958351
posite <100074242+posite@users.noreply.github.com>
2024-08-08 14:24:55 +0900

    [Gold III] Title: 연구소 3, Time: 312 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -1,99 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
-    static int min = Integer.MAX_VALUE;
+    private static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] board;
-        try {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int size = Integer.parseInt(st.nextToken());
-            int virusCount = Integer.parseInt(st.nextToken());
-            board = new int[size][size];
-            List<Point> viruses = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < size; j++) {
-                    board[i][j] = Integer.parseInt(st.nextToken());
-                    if (board[i][j] == 2) {
-                        viruses.add(new Point(i, j));
-                        board[i][j] = 3;
-                    }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] board = new int[n][n];
+        List<Point> viruses = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int stat = Integer.parseInt(st.nextToken());
+                if (stat == 2) {
+                    viruses.add(new Point(i, j));
+                    board[i][j] = 3;
+                } else {
+                    board[i][j] = stat;
                 }
             }
-            selectVirus(board, viruses, new ArrayList<>(), virusCount, 0);
-            if (min == Integer.MAX_VALUE) {
-                System.out.println("-1");
-            } else {
-                System.out.println(min);
-            }
-        } catch (IOException e) {
-
         }
+        selectVirus(new ArrayList<>(), viruses, board, n, m, 0);
+        if (min == Integer.MAX_VALUE) {
+            System.out.println("-1");
+        } else {
+            System.out.println(min);
+        }
+
     }
 
-    public static void selectVirus(int[][] board, List<Point> viruses, List<Point> now, int virusCount, int start) {
-        if (now.size() == virusCount) {
+    public static void selectVirus(List<Point> now, List<Point> viruses, int[][] board, int n, int m, int x) {
+        if (now.size() == m) {
             int[][] copy = new int[board.length][board[0].length];
-            for (int i = 0; i < copy.length; i++) {
-                System.arraycopy(board[i], 0, copy[i], 0, copy[0].length);
+            for (int i = 0; i < board.length; i++) {
+                System.arraycopy(board[i], 0, copy[i], 0, board[i].length);
             }
-            activateVirus(copy, now);
+            bfs(now, copy, n);
             return;
         }
 
-        for (int i = start; i < viruses.size(); i++) {
-            now.add(viruses.get(i));
-            selectVirus(board, viruses, now, virusCount, i + 1);
+        for (int i = x; i < viruses.size(); i++) { 
+            Point choice = viruses.get(i);
+            now.add(choice);
+            board[choice.x][choice.y] = 2;
+            selectVirus(now, viruses, board, n, m, i + 1);
             now.remove(now.size() - 1);
+            board[choice.x][choice.y] = 3;
         }
     }
 
-    public static void activateVirus(int[][] board, List<Point> now) {
-        int time = 0;
-        int length = board.length;
+    public static void bfs(List<Point> now, int[][] board, int n) {
+        int[][] times = new int[n][n];
+
         Deque<Point> queue = new ArrayDeque<>();
-        int[][] results = new int[length][length];
-        for (Point virus : now) {
-            board[virus.x][virus.y] = 2;
-            queue.add(virus);
-        }
+        for (Point virus : now) queue.add(virus);
 
         while (!queue.isEmpty()) {
-            Point current = queue.poll();
+            Point current = queue.remove();
             for (int i = 0; i < 4; i++) {
                 int nx = current.x + dx[i];
                 int ny = current.y + dy[i];
-                if (nx >= 0 && nx < length && ny >= 0 && ny < length) {
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
                     if (board[nx][ny] == 1 || board[nx][ny] == 2) continue;
-                    if (results[nx][ny] > results[current.x][current.y] + 1 || results[nx][ny] == 0) {
-                        results[nx][ny] = results[current.x][current.y] + 1;
-                        queue.offer(new Point(nx, ny));
+                    if (times[nx][ny] == 0 || times[nx][ny] > times[current.x][current.y] + 1) {
+                        times[nx][ny] = times[current.x][current.y] + 1;
+                        queue.add(new Point(nx, ny));
                     }
                 }
             }
         }
 
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
+
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == 0) {
-                    if (results[i][j] == 0) {
-                        return;
-                    }
+                    if (times[i][j] == 0) return;
+                    time = Math.max(times[i][j], time);
                 }
-                if (board[i][j] == 3) continue;
-                time = Math.max(time, results[i][j]);
             }
         }
-        /*for (int i = 0; i < length; i++) {
-            System.out.println(Arrays.toString(results[i]));
-        }
-        System.out.println();*/
-        min = Math.min(time, min);
+        min = Math.min(min, time);
     }
 
     public static class Point {
