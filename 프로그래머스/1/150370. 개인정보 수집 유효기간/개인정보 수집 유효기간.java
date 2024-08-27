e1070581b57aebf1e49822656ef101483cb90f09
posite <100074242+posite@users.noreply.github.com>
2024-08-27 10:24:05 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.22 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,31 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        Map<String, Long> typeMap = new HashMap<>();
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(today, ".");
+        Map<String, Long> types = new HashMap<>();
+        long todayValue = Long.parseLong(st.nextToken()) * 12 * 28 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
         for(String term: terms) {
             st = new StringTokenizer(term);
-            typeMap.put(st.nextToken(), Long.parseLong(st.nextToken()));
+            types.put(st.nextToken(), Long.parseLong(st.nextToken()));
         }
-        st = new StringTokenizer(today, ".");
-        long todayValue = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
-        
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<privacies.length; i++) {
-            st = new StringTokenizer(privacies[i]);
-            String date = st.nextToken();
+            String privacy = privacies[i];
+            st = new StringTokenizer(privacy);
+            String day = st.nextToken();
             String type = st.nextToken();
-            st = new StringTokenizer(date, ".");
-            
-            long dateValue = Long.parseLong(st.nextToken()) * 28 * 12 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + typeMap.get(type) * 28;
-            if(todayValue >= dateValue) {
-                list.add(i+1);
+            st = new StringTokenizer(day, ".");
+            long privacyValue = Long.parseLong(st.nextToken()) * 12 * 28 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + types.get(type) * 28;
+            if(todayValue >= privacyValue) {
+                list.add((i+1));
             }
         }
+        
+        
         int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        for(int i=0; i<list.size(); i++) answer[i] = list.get(i);
         return answer;
     }
 }
