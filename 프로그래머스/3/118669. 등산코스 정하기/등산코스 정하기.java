7015933c6751de90cb0dd36f380cb30a0cac33b9
posite <100074242+posite@users.noreply.github.com>
2024-08-27 16:16:15 +0900

    [level 3] Title: 등산코스 정하기, Time: 946.55 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -5,57 +5,57 @@ class Solution {
         int[] intensities = new int[n+1];
         Arrays.fill(intensities, Integer.MAX_VALUE);
         Arrays.sort(summits);
-        Queue<Point> pq = new PriorityQueue<>();
+        Map<Integer, List<Route>> map = new HashMap<>();
+        for(int i=1; i<=n; i++) {
+            map.put(i, new ArrayList<>());
+        }
+        for(int[] path: paths) {
+            map.get(path[0]).add(new Route(path[1], path[2]));
+            map.get(path[1]).add(new Route(path[0], path[2]));
+        }
+        Queue<Route> pq = new PriorityQueue<>();
         for(int gate: gates) {
-            pq.add(new Point(gate, 0));
+            pq.add(new Route(gate, 0));
             intensities[gate] = 0;
         }
-        Map<Integer, List<Point>> map = new HashMap<>();
-        for(int i=1; i<=n; i++) map.put(i, new ArrayList<>());
-        for(int[] path: paths) {
-            map.get(path[0]).add(new Point(path[1], path[2]));
-            map.get(path[1]).add(new Point(path[0], path[2]));
-        }
-        
         outer: while(!pq.isEmpty()) {
-            Point current = pq.remove();
-            if(current.intensity > intensities[current.node]) continue;
+            Route current = pq.remove();
             for(int summit: summits) {
-                if(current.node == summit) continue outer;
+                if(current.node == summit) {
+                    continue outer;
+                }
             }
-            
-            for(Point next: map.get(current.node)) {
-                int nextIntensity = Math.max(next.intensity, intensities[current.node]);
+            if(current.intensity > intensities[current.node]) continue;
+            for(Route next: map.get(current.node)) {
+                int nextIntensity = Math.max(next.intensity, current.intensity);
                 if(intensities[next.node] > nextIntensity) {
-                    pq.add(new Point(next.node, nextIntensity));
+                    pq.add(new Route(next.node, nextIntensity));
                     intensities[next.node] = nextIntensity;
                 }
             }
-            
         }
+        int index = 0;
         int min = Integer.MAX_VALUE;
-        int out = -1;
         for(int summit: summits) {
             if(min > intensities[summit]) {
-                out = summit;
                 min = intensities[summit];
+                index = summit;
             }
         }
-        
-        int[] answer = {out, min};
+        int[] answer = {index, min};
         return answer;
     }
     
-    public class Point implements Comparable<Point> {
+    public class Route implements Comparable<Route> {
         int node, intensity;
         
-        public Point(int node, int intensity) {
+        public Route(int node, int intensity) {
             this.node = node;
             this.intensity = intensity;
         }
         
         @Override
-        public int compareTo(Point o) {
+        public int compareTo(Route o) {
             return this.intensity - o.intensity;
         }
     }
