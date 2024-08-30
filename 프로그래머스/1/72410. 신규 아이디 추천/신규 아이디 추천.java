81f88a9bf61d7e0d73e286ba2eaf376548c24845
posite <100074242+posite@users.noreply.github.com>
2024-08-30 16:13:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.50 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,30 +1,24 @@
 class Solution {
     public String solution(String new_id) {
-        StringBuilder sb = new StringBuilder(new_id.toLowerCase());
-        for(int i=0; i<sb.length(); i++) {
-            char current = sb.charAt(i);
-            if(Character.isDigit(current) || Character.isLowerCase(current)) continue;
-            if(current == '-' || current == '_' || current == '.') continue;
-            sb.deleteCharAt(i);
+        StringBuilder answer = new StringBuilder(new_id.toLowerCase());
+        for(int i=0; i<answer.length(); i++) {
+            char current = answer.charAt(i);
+            if(Character.isLowerCase(current) || Character.isDigit(current)) continue;
+            if(current == '.' || current =='-' || current == '_') continue;
+            answer.deleteCharAt(i);
             i--;
         }
-        
-        while(sb.indexOf("..") != -1) {
-            int index = sb.indexOf("..");
-            sb.deleteCharAt(index+1);
+        while(answer.indexOf("..") != -1) {
+            int index = answer.indexOf("..");
+            answer.deleteCharAt(index+1);
         }
-        if(sb.charAt(0) == '.') sb.deleteCharAt(0);
-        if(sb.length() > 1) {
-            if(sb.charAt(sb.length() - 1) == '.') sb.deleteCharAt(sb.length() - 1);
-        }
-        if(sb.length() == 0) sb.append('a');
-        if(sb.length() > 15) sb.replace(15, sb.length(), "");
-        if(sb.length() > 1) {
-            if(sb.charAt(sb.length() - 1) == '.') sb.deleteCharAt(sb.length() - 1);
-        }
-        while(sb.length() < 3) {
-            sb.append(sb.charAt(sb.length() - 1));
-        }
-        return sb.toString();
+        if(answer.charAt(0) == '.') answer.deleteCharAt(0);
+        if(answer.length() > 0 && answer.charAt(answer.length()-1) == '.') answer.deleteCharAt(answer.length()-1);
+        if(answer.length() == 0) answer.append('a');
+        if(answer.length() > 15) answer.replace(15, answer.length(), "");
+        if(answer.charAt(0) == '.') answer.deleteCharAt(0);
+        if(answer.length() > 0 && answer.charAt(answer.length()-1) == '.') answer.deleteCharAt(answer.length()-1);
+        while(answer.length() < 3) answer.append(answer.charAt(answer.length()-1));
+        return answer.toString();
     }
 }
