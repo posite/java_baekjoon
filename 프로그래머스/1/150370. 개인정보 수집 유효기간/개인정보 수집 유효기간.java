cc433f2c365f0d786c66368c144d9331581053d0
posite <100074242+posite@users.noreply.github.com>
2024-09-05 21:33:54 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 8.62 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
+        Map<String, Long> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(today, ".");
-        Map<String, Long> types = new HashMap<>();
-        long todayValue = Long.parseLong(st.nextToken()) * 12 * 28 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken());
+        long todayValue = Long.parseLong(st.nextToken()) * 28* 12 + Long.parseLong(st.nextToken())*28 + Long.parseLong(st.nextToken());
+        System.out.println(todayValue);
         for(String term: terms) {
             st = new StringTokenizer(term);
-            types.put(st.nextToken(), Long.parseLong(st.nextToken()));
+            map.put(st.nextToken(), Long.parseLong(st.nextToken()));
         }
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<privacies.length; i++) {
-            String privacy = privacies[i];
-            st = new StringTokenizer(privacy);
-            String day = st.nextToken();
-            String type = st.nextToken();
-            st = new StringTokenizer(day, ".");
-            long privacyValue = Long.parseLong(st.nextToken()) * 12 * 28 + Long.parseLong(st.nextToken()) * 28 + Long.parseLong(st.nextToken()) + types.get(type) * 28;
-            if(todayValue >= privacyValue) {
-                list.add((i+1));
+            st = new StringTokenizer(privacies[i]);
+            String privacyDay = st.nextToken();
+            String term = st.nextToken();
+            st = new StringTokenizer(privacyDay, ".");
+            
+            long privacyValue = Long.parseLong(st.nextToken()) * 28* 12 + Long.parseLong(st.nextToken())*28 + Long.parseLong(st.nextToken()) + map.get(term)*28 -1;
+            System.out.println(privacyValue);
+            if(todayValue > privacyValue) {
+                list.add(i+1);
             }
         }
         
         
         int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++) answer[i] = list.get(i);
+        for(int i=0; i<answer.length; i++) answer[i] = list.get(i);
         return answer;
     }
 }
